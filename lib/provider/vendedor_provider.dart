b8b9fb1d511c737176d1c986599a9ac9c6a82d78
JesusVera1924 <desarrollodark@gmail.com>
2023-08-22 16:19:37 -0500

    cambioGenerarNc

--- lib/provider/vendedor_provider.dart
+++ lib/provider/vendedor_provider.dart
@@ -256,8 +256,7 @@ class VendedorProvider extends ChangeNotifier {
           }
         }
       } else {
-        customDialog1(context, "Extension permitida [pdf]",
-            Icons.warning_amber_rounded, Colors.red);
+        customDialog1(context, "Extension permitida [pdf]");
       }
     }
     return resp;
